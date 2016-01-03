8f3698baf1af861e12c2898727f6ee7424a49b52
Zhen Li <zhljen@gmail.com>
2016-01-02 22:03:21 -0800

    adding the mindmaps

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 var container = require("vertx/container");
+
 container.deployModule("io.vertx~mod-web-server~2.0.0-final", {
   port: 8080,
-  host: "localhost"
+  host: "localhost",
+  bridge: true,
+  inbound_permitted: [
+    { address: 'mindMaps.list' },
+    { address: 'mindMaps.save' },
+    { address: 'mindMaps.delete' }
+  ]
 });
 container.deployVerticle('mindmaps.js');
